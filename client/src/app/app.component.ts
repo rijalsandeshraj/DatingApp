d78b73c848cbf06bb5cefaeff3bcc07d3c71c386
Sandesh Rijal <rijalsandeshraj@example.com>
2024-07-29 14:42:09 +0545

    Successfully implemented authentication on the client side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, Injectable, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -15,13 +18,28 @@ export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('http://localhost:5000/api/users').subscribe({
       next: (response) => (this.users = response),
       error: (error) => console.log(error),
       complete: () => console.log('Request has been completed'),
     });
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { CommonModule, NgIf } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgIf, BsDropdownModule],
+  providers: [provideAnimations()],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css',
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public accountService: AccountService) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  logOut() {
+    this.accountService.logOut();
+  }
+}
